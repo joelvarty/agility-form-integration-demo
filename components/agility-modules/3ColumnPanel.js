454260f02f6e3762deb755f39bf72d6d987a73f8
Joel Varty <joel@agilitycms.com>
2021-02-09 11:45:50 -0500

    added 3 column panel

---
+++ components/agility-modules/3ColumnPanel.js
@@ -0,0 +1,85 @@
+import truncate from 'truncate-html'
+
+import ThreeColSlider from "components/cards/ThreeColSlider"
+
+const ThreeColumnPanel =  ({customData, fields, page}) => {
+
+
+
+	// const cards = [
+	// 	{
+	// 	  imageSrc: "https://images.unsplash.com/photo-1571896349842-33c89424de2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80",
+	// 	  title: "Wyatt Residency",
+	// 	  description: "Lorem ipsum dolor sit amet, consectur dolori adipiscing elit, sed do eiusmod tempor nova incididunt ut labore et dolore magna aliqua.",
+	// 	  locationText: "Rome, Italy",
+	// 	  pricingText: "USD 39/Day",
+	// 	  rating: "4.8",
+	// 	},
+	// 	{
+	// 	  imageSrc: "https://images.unsplash.com/photo-1571003123894-1f0594d2b5d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80",
+	// 	  title: "Soho Paradise",
+	// 	  description: "Lorem ipsum dolor sit amet, consectur dolori adipiscing elit, sed do eiusmod tempor nova incididunt ut labore et dolore magna aliqua.",
+	// 	  locationText: "Ibiza, Spain",
+	// 	  pricingText: "USD 50/Day",
+	// 	  rating: 4.9,
+	// 	},
+	// 	{
+	// 	  imageSrc: "https://images.unsplash.com/photo-1549294413-26f195200c16?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80",
+	// 	  title: "Hotel Baja",
+	// 	  description: "Lorem ipsum dolor sit amet, consectur dolori adipiscing elit, sed do eiusmod tempor nova incididunt ut labore et dolore magna aliqua.",
+	// 	  locationText: "Palo Alto, CA",
+	// 	  pricingText: "USD 19/Day",
+	// 	  rating: "5.0",
+	// 	},
+	// 	{
+	// 	  imageSrc: "https://images.unsplash.com/photo-1571770095004-6b61b1cf308a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80",
+	// 	  title: "Hudak Homes",
+	// 	  description: "Lorem ipsum dolor sit amet, consectur dolori adipiscing elit, sed do eiusmod tempor nova incididunt ut labore et dolore magna aliqua.",
+	// 	  locationText: "Arizona, RAK",
+	// 	  pricingText: "USD 99/Day",
+	// 	  rating: 4.5,
+	// 	},
+	//   ]
+
+	return <ThreeColSlider { ... customData}   />
+}
+
+
+ThreeColumnPanel.getCustomInitialProps = async function ({agility, item, channelName, languageCode}) {
+
+
+	try {
+
+		//then get our posts
+		const api = agility;
+
+		//then get our posts
+		let cardsContentList = await api.getContentList({ referenceName: item.fields.cards.referencename, languageCode });
+
+
+		let cards = cardsContentList.map(c => {
+			return {
+				title: c.fields.title,
+				regionTarget: c.fields.regionTarget,
+				description: c.fields.description,
+				imageSrc: `${c.fields.image.url}?w=400&h=400` ,
+				locationText: c.fields.tagDescription,
+				pricingText: c.fields.pricingText,
+				rating: c.fields.rating
+			}
+		})
+
+		return {
+			title: item.fields.title,
+			cards
+		}
+
+
+	} catch (error) {
+		if (console) console.error(error);
+	}
+
+
+}
+
+export default ThreeColumnPanel;
--- components/cards/ThreeColSlider.js
+++ components/cards/ThreeColSlider.js
@@ -27,7 +27,7 @@ const NextButton = tw(ControlButton)``;
 
 const CardSlider = styled(Slider)`
   ${tw`mt-16`}
-  .slick-track { 
+  .slick-track {
     ${tw`flex`}
   }
   .slick-slide {
@@ -65,7 +65,7 @@ const IconContainer = styled.div`
 const Text = tw.div`ml-2 text-sm font-semibold text-gray-800`;
 
 const PrimaryButton = tw(PrimaryButtonBase)`mt-auto sm:text-lg rounded-none w-full rounded sm:rounded-none sm:rounded-br-4xl py-3 sm:py-6`;
-export default () => {
+export default ({title, cards}) => {
   // useState is used instead of useRef below because we want to re-render when sliderRef becomes available (not null)
   const [sliderRef, setSliderRef] = useState(null);
   const sliderSettings = {
@@ -89,46 +89,13 @@ export default () => {
   };
 
   /* Change this according to your needs */
-  const cards = [
-    {
-      imageSrc: "https://images.unsplash.com/photo-1571896349842-33c89424de2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80",
-      title: "Wyatt Residency",
-      description: "Lorem ipsum dolor sit amet, consectur dolori adipiscing elit, sed do eiusmod tempor nova incididunt ut labore et dolore magna aliqua.",
-      locationText: "Rome, Italy",
-      pricingText: "USD 39/Day",
-      rating: "4.8",
-    },
-    {
-      imageSrc: "https://images.unsplash.com/photo-1571003123894-1f0594d2b5d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80",
-      title: "Soho Paradise",
-      description: "Lorem ipsum dolor sit amet, consectur dolori adipiscing elit, sed do eiusmod tempor nova incididunt ut labore et dolore magna aliqua.",
-      locationText: "Ibiza, Spain",
-      pricingText: "USD 50/Day",
-      rating: 4.9,
-    },
-    {
-      imageSrc: "https://images.unsplash.com/photo-1549294413-26f195200c16?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80",
-      title: "Hotel Baja",
-      description: "Lorem ipsum dolor sit amet, consectur dolori adipiscing elit, sed do eiusmod tempor nova incididunt ut labore et dolore magna aliqua.",
-      locationText: "Palo Alto, CA",
-      pricingText: "USD 19/Day",
-      rating: "5.0",
-    },
-    {
-      imageSrc: "https://images.unsplash.com/photo-1571770095004-6b61b1cf308a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80",
-      title: "Hudak Homes",
-      description: "Lorem ipsum dolor sit amet, consectur dolori adipiscing elit, sed do eiusmod tempor nova incididunt ut labore et dolore magna aliqua.",
-      locationText: "Arizona, RAK",
-      pricingText: "USD 99/Day",
-      rating: 4.5,
-    },
-  ]
+
 
   return (
     <Container>
       <Content>
         <HeadingWithControl>
-          <Heading>Popular Hotels</Heading>
+          <Heading>{title}</Heading>
           <Controls>
             <PrevButton onClick={sliderRef?.slickPrev}><ChevronLeftIcon/></PrevButton>
             <NextButton onClick={sliderRef?.slickNext}><ChevronRightIcon/></NextButton>
