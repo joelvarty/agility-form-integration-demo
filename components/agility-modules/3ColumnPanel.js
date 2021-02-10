69813feb118df4d7f0bc6be822d01f0fcd8f9adc
Joel Varty <joel@agilitycms.com>
2021-02-10 16:53:53 -0500

    15

--- components/agility-modules/3ColumnPanel.js
+++ components/agility-modules/3ColumnPanel.js
@@ -5,6 +5,8 @@ import React from 'react'
 import { load } from '@agility/content-sync'
 
 const ThreeColumnPanel =  ({customData, fields, page}) => {
+	const [variant, setVariant] = React.useState(-1)
+	const [cards, setCards] = React.useState([])
 
 	React.useEffect(() => {
 		if (typeof (window) === undefined) return
@@ -16,88 +18,24 @@ const ThreeColumnPanel =  ({customData, fields, page}) => {
 			}
 
 			console.log("checking for experiments")
-			const ret = window.google_optimize.get("-sAuWoxbRZ2w8JSLlOFnbQ", (cbRet) => {
-				console.log("cbRet", cbRet)
-			});
-	  		console.log("ret", ret)
-
-			// console.log("checking for experiments")
-			// window.gtag('event', 'optimize.callback', {
-			// 	name: "-sAuWoxbRZ2w8JSLlOFnbQ",
-			// 	callback: (combination, experimentId, containerId) => {
-			// 		console.log("Optimize callback!", combination, experimentId, containerId);
-
-			// 		const ret = google_optimize.get(experimentId, (cbRet) => {
-			// 		 		 console.log("cbRet", cbRet)
-			// 		 	 });
-			// 		console.log("ret", ret)
-			// 	}
-			//  });
+			const variantIndex = window.google_optimize.get(customData.experimentID)
+	  		console.log({variantIndex})
+			if (variantIndex > 0) {
+				const filteredCards = customData.cards.filter(c => c.variant == variantIndex)
+				setCards(filteredCards)
+			} else {
+				setCards(customData.cards)
+			}
 		}
 
 		setTimeout(loadExperiment, 0)
 
-
-
-		// gtag('event', 'optimize.callback', {
-		// 	callback: (combination, experimentId, containerId) => {
-
-		// 	 console.log({combination, experimentId, containerId});
-		// 	 const ret = google_optimize.get(experimentId, (cbRet) => {
-		// 		 console.log("cbRet", cbRet)
-		// 	 });
-		// 	 console.log("ret", ret)
-		// 	}
-		//  });
-
-
-		// 	if (window.google_optimize !== undefined) {
-		// 	  const variant = window.google_optimize.get("DRtR7BqNTv-w1vaaipidFQ");
-		// 	  this.setState({ variant });
-		// 	  clearInterval(this.intervalId);
-		// 	}
-		//   }, 100);
-
-
 	}, [])
 
 
-	// const cards = [
-	// 	{
-	// 	  imageSrc: "https://images.unsplash.com/photo-1571896349842-33c89424de2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80",
-	// 	  title: "Wyatt Residency",
-	// 	  description: "Lorem ipsum dolor sit amet, consectur dolori adipiscing elit, sed do eiusmod tempor nova incididunt ut labore et dolore magna aliqua.",
-	// 	  locationText: "Rome, Italy",
-	// 	  pricingText: "USD 39/Day",
-	// 	  rating: "4.8",
-	// 	},
-	// 	{
-	// 	  imageSrc: "https://images.unsplash.com/photo-1571003123894-1f0594d2b5d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80",
-	// 	  title: "Soho Paradise",
-	// 	  description: "Lorem ipsum dolor sit amet, consectur dolori adipiscing elit, sed do eiusmod tempor nova incididunt ut labore et dolore magna aliqua.",
-	// 	  locationText: "Ibiza, Spain",
-	// 	  pricingText: "USD 50/Day",
-	// 	  rating: 4.9,
-	// 	},
-	// 	{
-	// 	  imageSrc: "https://images.unsplash.com/photo-1549294413-26f195200c16?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80",
-	// 	  title: "Hotel Baja",
-	// 	  description: "Lorem ipsum dolor sit amet, consectur dolori adipiscing elit, sed do eiusmod tempor nova incididunt ut labore et dolore magna aliqua.",
-	// 	  locationText: "Palo Alto, CA",
-	// 	  pricingText: "USD 19/Day",
-	// 	  rating: "5.0",
-	// 	},
-	// 	{
-	// 	  imageSrc: "https://images.unsplash.com/photo-1571770095004-6b61b1cf308a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80",
-	// 	  title: "Hudak Homes",
-	// 	  description: "Lorem ipsum dolor sit amet, consectur dolori adipiscing elit, sed do eiusmod tempor nova incididunt ut labore et dolore magna aliqua.",
-	// 	  locationText: "Arizona, RAK",
-	// 	  pricingText: "USD 99/Day",
-	// 	  rating: 4.5,
-	// 	},
-	//   ]
-
-	return <ThreeColSlider { ... customData}   />
+	if (variant < 0) {
+		return <ThreeColSlider title={customData.title} cards={cards}  />
+	}
 }
 
 
@@ -113,9 +51,11 @@ ThreeColumnPanel.getCustomInitialProps = async function ({agility, item, channel
 		let cardsContentList = await api.getContentList({ referenceName: item.fields.cards.referencename, languageCode });
 
 
+
 		let cards = cardsContentList.map(c => {
 			return {
 				id: c.contentID,
+				variant: c.fields.variant,
 				title: c.fields.title,
 				regionTarget: c.fields.regionTarget,
 				description: c.fields.description,
@@ -128,6 +68,7 @@ ThreeColumnPanel.getCustomInitialProps = async function ({agility, item, channel
 
 		return {
 			title: item.fields.title,
+			experimentID: item.fields.experimentID || null,
 			cards
 		}
 
